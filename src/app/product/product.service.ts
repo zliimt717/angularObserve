55e60f7fc49dd61afee27cce54de2e88480b04ad
xin.gu <xin.gu@accenture.com>
2022-11-24 09:51:24 +0100

    add dolar symbol and mapping http response

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, tap, throwError } from 'rxjs';
+import { catchError, map, Observable, tap, throwError } from 'rxjs';
 import { Product } from './product';
 
 @Injectable({
@@ -12,6 +12,13 @@ export class ProductService {
 
   products$=this.http.get<Product[]>(this.productsUrl)
   .pipe(
+    map(products=>
+      products.map(product=>
+        ({
+          ...product,
+          price:product.price?product.price*1.5:0,
+          searchKey:[product.productName]
+        } as Product))),
     tap(data=>console.log('Products:',JSON.stringify(data))),
     catchError(this.handleError)
   );
